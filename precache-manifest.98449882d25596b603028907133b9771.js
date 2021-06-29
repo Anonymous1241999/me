472841283ebb51ff366b0a97b749a706c666d42f
Anonymous1241999 <56948540+Anonymous1241999@users.noreply.github.com>
2021-06-29 21:19:23 +0530

    Updates

---
+++ precache-manifest.98449882d25596b603028907133b9771.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0129a1e6f761bf3b331",
+    "url": "/mern/static/css/main.7b5ee82d.chunk.css"
+  },
+  {
+    "revision": "c0129a1e6f761bf3b331",
+    "url": "/mern/static/js/main.c0129a1e.chunk.js"
+  },
+  {
+    "revision": "758aecd25a7568d25ef7",
+    "url": "/mern/static/js/runtime~main.758aecd2.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/mern/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "b2ad22ae3b26241b77133bad84dc6f0d",
+    "url": "/mern/index.html"
+  }
+];
