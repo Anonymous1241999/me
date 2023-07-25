408aae8283d2df728478c09bedfecd83c1bf6d62
Anonymous1241999 <56948540+Anonymous1241999@users.noreply.github.com>
2023-07-25 16:43:02 +0530

    Updates

---
+++ precache-manifest.fac0994448d3c7bb584bd837e6d61ff0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbfad4eeddcb7833a141",
+    "url": "/me/static/css/main.38641e6d.chunk.css"
+  },
+  {
+    "revision": "bbfad4eeddcb7833a141",
+    "url": "/me/static/js/main.bbfad4ee.chunk.js"
+  },
+  {
+    "revision": "70477d4a612a629a76b9",
+    "url": "/me/static/js/1.70477d4a.chunk.js"
+  },
+  {
+    "revision": "f4ae667e0f9864d95541",
+    "url": "/me/static/js/runtime~main.f4ae667e.js"
+  },
+  {
+    "revision": "fb41d87418627a76dd6eceb47db45de1",
+    "url": "/me/index.html"
+  }
+];
